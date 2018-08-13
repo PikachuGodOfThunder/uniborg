673b3c3622bbfd8d9e7f780d362dd60d719f21c9
Abin Paul Zachariah <39844254+AbinPaulZackariah@users.noreply.github.com>
2018-08-14 02:46:36 +0530

    Fak dis plugin i can’t fix dis 😣😣😢

--- stdplugins/bluetext.py
+++
@@ -1,9 +0,0 @@
-# First plugin by Abin Paul Zackariah Hope this one will work
-
-from telethon import events
-
-@borg.on(events.NewMessage(pattern=r".bluetext", outgoing=True))
-async def _(event):
-   if event.fwd_from:
-        return
-  await event.edit("BLUE TEXT\n MUST CLICK\n I AM A STUPID ANIMAL THAT IS ATTRACTED TO COLORS")
