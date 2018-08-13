6aa7f158fe03e041845f2ac8507305ae13d9020b
Abin Paul Zachariah <39844254+AbinPaulZackariah@users.noreply.github.com>
2018-08-14 02:22:43 +0530

    Create bluetext.py

---
+++ stdplugins/bluetext.py
@@ -0,0 +1,9 @@
+# First plugin by Abin Paul Zackariah Hope this one will work
+
+from telethon import events
+
+@borg.on(events.NewMessage(pattern=r".bluetext", outgoing=True))
+async def _(event):
+   if event.fwd_from
+        return
+   await.event_edit("BLUE TEXT\n MUST CLICK\n I AM A STUPID ANIMAL THAT IS ATTRACTED TO COLORS")
