5c286267547a15c160ae79e96dcdc4384be14e17
Abin Paul Zachariah <39844254+AbinPaulZackariah@users.noreply.github.com>
2018-08-14 02:43:44 +0530

    fixed another error  😣😒

--- stdplugins/bluetext.py
+++ stdplugins/bluetext.py
@@ -4,6 +4,6 @@ from telethon import events
 
 @borg.on(events.NewMessage(pattern=r".bluetext", outgoing=True))
 async def _(event):
-   if event.fwd_from
+   if event.fwd_from:
         return
   await event.edit("BLUE TEXT\n MUST CLICK\n I AM A STUPID ANIMAL THAT IS ATTRACTED TO COLORS")
